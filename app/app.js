f38158a875e2626b1049d93c2cf2331bdfaabd1f
Donna Lorenzo <dlorenzo24@hotmail.com>
2021-10-14 10:07:44 -0500

    removed api key

--- app/app.js
+++ app/app.js
@@ -40,7 +40,7 @@ let nextDay5 = [];
 // Store API URl & Key
 const forcastURl = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
 const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '&appid=05b53c214ae94f4e4dbf2c0830ee07b1';
+const apiKey = 'your api key here';
 let searchQuery;
 
 // The Date
